135df1116238f2681c16236768c02cf868b486a0
khorëm subhaní <ksubhani@globeandmail.com>
2024-03-22 11:30:49 -0400

    updated the script with help, list, and change functionalities

--- mass_enable_srv_checks/run.sh
+++ mass_enable_srv_checks/run.sh
@@ -1,10 +1,21 @@
 #!/usr/local/bin/bash
 # ksubhani@globeandmail.com
+# 20240321
+# This script was created to enable service checks in mass
 
 now=`date +%s`
 commandfile='/usr/local/nagios/var/rw/nagios.cmd'
 
-hostsrvs="$(wget -q -O - https://nagios.theglobeandmail.com/cgi/status.cgi?host=all | grep "Active checks of the service have been disabled" | sed -n "s/.*host=\([^&]*\)&srv=\([^']*\).*/\1 - \2/p")"
+userinput="$1"
+
+nagiosurl="https://nagios.theglobeandmail.com/cgi/status.cgi?host=all"
+hostsrvs="$(wget -q -O - $nagiosurl | grep "Active checks of the service have been disabled" | sed -n "s/.*host=\([^&]*\)&srv=\([^']*\).*/\1 - \2/p")"
+
+runhelp() {
+  cat << EOF
+usage: $0 [-l|--list] [-c|--change] [-h|--help]
+EOF
+}
 
 printresult() {
     fmt="%9s: %s \n%9s: %s \n%9s: %s\n\n"
@@ -15,18 +26,42 @@ enablesvc() {
         $(/bin/printf "[%lu] ENABLE_SVC_CHECK;$1;$2\n" $now > $commandfile)
 }
 
-main() {
-  IFS=$'\n'
+check() {
   if [[ ! -z ${hostsrvs} ]]; then
+    IFS=$'\n'
     for line in ${hostsrvs} ; do
       host="$(echo $line | awk -F ' - ' '{print $1}')"
       service="$(echo $line | awk -F ' - ' '{sub(/^[^-]+ - /, ""); print}')"
-      enablesvc "$host" "$service"
+      if [[ $userinput == "-c" ]] || [[ $userinput == "--change" ]]; then
+        enablesvc "$host" "$service"
+      fi
       printresult "$host" "$service"
     done
   else
-    echo "All hosts are enabled... nothing to do"
+    echo "nothing to do... all hosts are already enabled."
   fi
+}
+
+main() {
+  case $userinput in
+
+    -l | --list)
+      check "$userinput"
+      ;;
+
+    -c | --change)
+      check "$userinput"
+      ;;
+
+    -h | --help)
+      runhelp
+      ;;
+
+    *)
+      printf "[I]nvalid input: try using -h flag for help\n"
+      ;;
+  esac
+  shift
 }
 
 main
